603e237502fccf58ce21832c87f8cbcf6e6f9eca
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-03-01 12:05:18 -0500

    Avance ejercio 5 no finalizado aun 2.0--

--- src/VISTA/NewMain5.java
+++ src/VISTA/NewMain5.java
@@ -11,7 +11,23 @@ package VISTA;
  * sus especificaciones, características y su código en Python 3. Comparativa final entre los
  * algoritmos de ordenamiento. https://github.com/gbaudino/MetodosDeOrdenamiento
  */
+
+import MODELO.NewClass5;
+
 public class NewMain5 {
-    public static void main (String[]args){
+    public static void main(String[] args) {
+        NewClass5 modelo = new NewClass5();
+        int[] vector = modelo.generarVector();
+
+        modelo.mostrarVector(vector, "Vector Original:");
+
+        int[] vectorSelection = vector.clone();
+        modelo.selectionSort(vectorSelection);
+        modelo.mostrarVector(vectorSelection, "Ordenado con SelectionSort:");
+
+        int[] vectorQuick = vector.clone();
+        modelo.quickSort(vectorQuick, 0, vectorQuick.length - 1);
+        modelo.mostrarVector(vectorQuick, "Ordenado con QuickSort:");
     }
 }
+
